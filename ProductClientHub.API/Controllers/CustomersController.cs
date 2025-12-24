6660737dc18a66e6e3d91736d0b25c58c95b4518
Mori_ <Mori_@TAKEO>
2025-12-24 00:58:49 -0300

    Adicionar arquivos de projeto.

---
+++ ProductClientHub.API/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+using System.Net.Cache;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody]RequestClientJson request)
+        {
+            return Created();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        //Necessidade em informar se recebe ou não o parâmetro, caso contrário
+        //o C# vai disparar um erro, pois ele não vai saber identificar qual chamar
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ProductClientHub.Communication/Requests/ResquestClientJson.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductClientHub.Communication.Requests
+{
+    public class RequestClientJson
+
+    {
+        public String Name { get; set; } = string.Empty;
+        public String Email { get; set; } = string.Empty;
+    }
+}
