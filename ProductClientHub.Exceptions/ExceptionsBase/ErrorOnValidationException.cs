69b04a3ea21652d143ad3e84d049b925d6d642c5
Lucas Mori <morilucasaluno@gmail.com>
2025-12-29 02:27:54 -0300

    feat: imeplementar novas features e tratamentos

---
+++ ProductClientHub.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,24 @@
+﻿using ProductClientHub.Exceptions.ExceptionBase;
+using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase
+{
+    public class ErrorOnValidationException : ProductsClientHubException
+    {
+        private readonly List<string> _errors;
+        public ErrorOnValidationException(List<string> errorMessage) : base (string.Empty)
+        {
+            _errors = errorMessage;
+        }
+
+        public override List<string> GetErrors()
+        {
+            return _errors;
+        }
+
+        public override HttpStatusCode GetHttpStatusCode()
+        {
+            return HttpStatusCode.BadRequest;
+        }
+    }
+}
